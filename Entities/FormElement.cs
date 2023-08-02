7f68977d498ff3f131dbbc2197a3fa844596a5eb
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-02 15:04:44 +0330

    change options type from string to list of string

--- Entities/FormElement.cs
+++ Entities/FormElement.cs
@@ -4,7 +4,7 @@ public class FormElement
 {
     public int Id { get; set; }
     public string Label { get; set; }
-    public string Options { get; set; }
+    public List<string> Options { get; set; }
     public virtual Form Form { get; set; }
     public FormElementType Type { get; set; }
     
