f9205f3a2dc18cb0442ba326c083ebaa2309983f
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-05 18:11:26 +0330

    options is not a list

--- Entities/FormElement.cs
+++ Entities/FormElement.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FormGeneratorAPI.Entities;
 
 public class FormElement
 {
     public int Id { get; set; }
     public string Label { get; set; }
-    public List<string> Options { get; set; }
+    public string Options { get; set; } = "";
     public virtual Form Form { get; set; }
     public FormElementType Type { get; set; }
-    
+
 }
