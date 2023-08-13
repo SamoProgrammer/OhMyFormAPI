869bb8926c5e1d40345d3296ce7d6e714d5bbbb1
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-13 22:44:38 +0330

    options from list to string

--- DTOs/FormElement/UpdateFormElementModel.cs
+++ DTOs/FormElement/UpdateFormElementModel.cs
@@ -3,10 +3,9 @@ using FormGeneratorAPI.Entities;
 namespace FormGeneratorAPI.DTOs.FormElement;
 
 public class UpdateFormElementModel
-{
-    public int Id { get; set; }
+{   public int Id { get; set; }
     public string Label { get; set; }
-    public List<string> Options { get; set; }
-    public int FormId { get; set; }
+    public string Options { get; set; }
     public FormElementType Type { get; set; }
+    public int FormId { get; set; }
 }
