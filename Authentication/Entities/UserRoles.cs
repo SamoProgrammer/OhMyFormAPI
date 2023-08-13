87d70130fe6a349a8e55f099b2690019c5197645
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-13 22:43:25 +0330

    fix roles

--- Authentication/Entities/UserRoles.cs
+++ Authentication/Entities/UserRoles.cs
@@ -2,6 +2,7 @@ namespace FormGeneratorAPI.Authentication;
 
 public class UserRoles
 {
-    public const string SuperAdmin = "SuperAdmin";
+    // public const string SuperAdmin = "SuperAdmin";
     public const string Admin = "Admin";
+    public const string User = "User";
 }
